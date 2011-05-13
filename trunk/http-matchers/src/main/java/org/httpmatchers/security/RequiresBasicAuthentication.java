e01850d9845e18689b0757126f664c3840ea1267
david.ehringer@gmail.com <david.ehringer@gmail.com@837e4179-004c-dfba-547e-721e38d796ae>
2011-05-13 03:50:31 +0000

    Moved an abstract base class in an internal package.

--- trunk/http-matchers/src/main/java/org/httpmatchers/security/RequiresBasicAuthentication.java
+++ trunk/http-matchers/src/main/java/org/httpmatchers/security/RequiresBasicAuthentication.java
@@ -22,7 +22,7 @@ import org.apache.http.client.methods.HttpGet;
 import org.hamcrest.Description;
 import org.hamcrest.Factory;
 import org.hamcrest.Matcher;
-import org.httpmatchers.BaseHttpMatcher;
+import org.httpmatchers.internal.BaseHttpMatcher;
 
 /**
  * @author David Ehringer
