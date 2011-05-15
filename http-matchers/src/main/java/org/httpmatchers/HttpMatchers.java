a604bb15ae9a85a1b6b9a59ce52eebeb027c8b78
david.ehringer@gmail.com <david.ehringer@gmail.com@837e4179-004c-dfba-547e-721e38d796ae>
2011-05-15 19:21:57 +0000

    Added an alternative form of credential verification.

--- http-matchers/src/main/java/org/httpmatchers/HttpMatchers.java
+++ http-matchers/src/main/java/org/httpmatchers/HttpMatchers.java
@@ -3,34 +3,28 @@ package org.httpmatchers;
 
 public class HttpMatchers {
 
-	public static org.hamcrest.Matcher<java.lang.String> isAccessible() {
-		return org.httpmatchers.access.UrlIsAccessible.isAccessible();
-	}
+  public static org.hamcrest.Matcher<java.lang.String> isAccessible() {
+    return org.httpmatchers.access.UrlIsAccessible.isAccessible();
+  }
 
-	public static org.hamcrest.Matcher<java.lang.String> isAccessible(
-			org.httpmatchers.security.Credentials credentials) {
-		return org.httpmatchers.access.UrlIsAccessible
-				.isAccessible(credentials);
-	}
+  public static org.hamcrest.Matcher<java.lang.String> isAccessible(org.httpmatchers.security.Credentials credentials) {
+    return org.httpmatchers.access.UrlIsAccessible.isAccessible(credentials);
+  }
 
-	public static org.hamcrest.Matcher<java.lang.String> hasCertificateExpiringInDays(
-			org.hamcrest.Matcher<? super java.lang.Integer> daysMatcher) {
-		return org.httpmatchers.security.CertificateExpiration
-				.hasCertificateExpiringInDays(daysMatcher);
-	}
+  public static org.hamcrest.Matcher<java.lang.String> hasCertificateExpiringInDays(org.hamcrest.Matcher<? super java.lang.Integer> daysMatcher) {
+    return org.httpmatchers.security.CertificateExpiration.hasCertificateExpiringInDays(daysMatcher);
+  }
 
-	public static org.hamcrest.Matcher<org.httpmatchers.security.Credentials> areAuthorizedFor(
-			java.lang.String url) {
-		return org.httpmatchers.security.CredentialsAreAuthorized
-				.areAuthorizedFor(url);
-	}
+  public static org.hamcrest.Matcher<org.httpmatchers.security.Credentials> areAuthorizedFor(java.lang.String url) {
+    return org.httpmatchers.security.CredentialsAreAuthorized.areAuthorizedFor(url);
+  }
 
-	public static org.hamcrest.Matcher<java.lang.String> requiresBasicAuthentication() {
-		return org.httpmatchers.security.RequiresBasicAuthentication
-				.requiresBasicAuthentication();
-	}
+  public static org.hamcrest.Matcher<java.lang.String> requiresBasicAuthentication() {
+    return org.httpmatchers.security.RequiresBasicAuthentication.requiresBasicAuthentication();
+  }
 
-	public static org.hamcrest.Matcher<java.lang.String> requiresSsl() {
-		return org.httpmatchers.security.RequiresSsl.requiresSsl();
-	}
+  public static org.hamcrest.Matcher<java.lang.String> requiresSsl() {
+    return org.httpmatchers.security.RequiresSsl.requiresSsl();
+  }
+
 }
